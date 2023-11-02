9fe1f0db878fe2838bbecfa09293455303c17842
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-03 02:35:13 +0600

    Appication user model added

---
+++ social-network-platform-server.Domain/Accounts/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using social_network_platform_server.Domain.AuditEntitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Domain.Accounts
+{
+    public class ApplicationUser : AuditEntity
+    { 
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
