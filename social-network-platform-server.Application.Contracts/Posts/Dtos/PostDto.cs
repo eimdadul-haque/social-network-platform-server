b614a9c57f26315ed8833a77d3e7b26efdc542b0
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-04 11:56:01 +0600

    Post Dto Added

---
+++ social-network-platform-server.Application.Contracts/Posts/Dtos/PostDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Application.Contracts.Posts.Dtos
+{
+    public class PostDto : AuditEntityDto<Guid>
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Guid AuthorID { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<Like> Likes { get; set; }
+    }
+}
