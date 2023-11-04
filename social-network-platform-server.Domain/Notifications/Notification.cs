b65fe34a2495dc65c70deb96036eeedff1a1b6cd
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-04 11:50:52 +0600

    notification entity added

---
+++ social-network-platform-server.Domain/Notifications/Notification.cs
@@ -0,0 +1,19 @@
+﻿using social_network_platform_server.Domain.Accounts;
+using social_network_platform_server.Domain.AuditEntitys;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace social_network_platform_server.Domain.Notifications
+{
+    public class Notification : AuditEntity
+    {
+        public int MyProperty { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime NotificationDate { get; set; }
+
+        public Guid UserID { get; set; }
+        [ForeignKey("UserID")]
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
