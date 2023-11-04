f56a7faa181552d5254772dcc69068f054ee3ace
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-04 11:52:08 +0600

    generic repository added

---
+++ social-network-platform-server.Application.Contracts/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Application.Contracts.Repository.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class 
+    {
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<List<TEntity>> AddRangeAsync(List<TEntity> entitys);
+        Task<TEntity> GetByIdAsync(Guid Id);
+        Task<List<TEntity>> GetAsync(Guid Id);
+        Task<bool> DeleteAsync(Guid Id, bool isSoft);
+    }
+}
---
+++ social-network-platform-server.Application/Repositorys/Repository.cs
@@ -0,0 +1,37 @@
+﻿using social_network_platform_server.Application.Contracts.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Application.Repositorys
+{
+    public class Repository<TEntity> where TEntity : class, IRepository<TEntity>
+    {
+        public Task<TEntity> AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TEntity>> AddRangeAsync(List<TEntity> entitys)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(Guid Id, bool isSoft)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TEntity>> GetAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
