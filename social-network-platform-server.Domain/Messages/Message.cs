f5a4c06e15058b063055fd0712cd4a054c142163
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-04 11:50:30 +0600

    message entity added

---
+++ social-network-platform-server.Domain/Messages/Message.cs
@@ -0,0 +1,17 @@
+﻿using social_network_platform_server.Domain.AuditEntitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Domain.Messages
+{
+    public class Message : AuditEntity
+    {
+        public Guid SenderID { get; set; }
+        public Guid RecipientID { get; set; }
+        public string Content { get; set; }
+        public DateTime MessageDate { get; set; }
+    }
+}
