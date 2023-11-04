623a831b6a0c4d55718f04481b46beec9116cfed
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-04 11:53:11 +0600

    post service added

---
+++ social-network-platform-server.Application.Contracts/Posts/Interfaces/IPostService.cs
@@ -0,0 +1,36 @@
+﻿using social_network_platform_server.Application.Contracts.Posts.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Application.Contracts.Posts.Interfaces
+{
+    public interface IPostService
+    {
+        //Enables users to create new posts.
+        Task<PostDto> CreatePost(PostDto entity);
+
+        //Retrieves details of a specific post.
+        Task<PostDto> GetPost(Guid Id);
+
+        //Allows users to edit their own posts.
+        Task<PostDto> EditPost(PostDto entity);
+
+        //Deletes a user's own post.
+        Task<bool> DeletePost(Guid Id);
+
+        //Retrieves posts created by a specific user.
+        Task<List<PostDto>> GetUserPosts(Guid userId);
+
+        //Displays posts from friends in the user's network.
+        Task<List<PostDto>> GetFriendPosts(Guid userId);
+
+        //Lets users like or react to a post.
+        Task<bool> LikePost(PostDto entity);
+
+        //Allows users to add comments to a post.
+        Task<bool> AddCommentToPost(PostDto entity);
+    }
+}
---
+++ social-network-platform-server.Application/Posts/PostService.cs
@@ -0,0 +1,62 @@
+﻿using social_network_platform_server.Application.Contracts.Posts.Dtos;
+using social_network_platform_server.Application.Contracts.Posts.Interfaces;
+using social_network_platform_server.Application.Contracts.Repository.Interfaces;
+using social_network_platform_server.Domain.Likes;
+using social_network_platform_server.Domain.Posts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Application.Posts
+{
+    public class PostService : IPostService
+    {
+        private readonly IRepository<Post> _postRepository;
+        public PostService(
+            IRepository<Post> postRepository)
+        {
+            _postRepository = postRepository;
+        }
+        public Task<bool> AddCommentToPost(PostDto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostDto> CreatePost(PostDto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeletePost(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostDto> EditPost(PostDto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<PostDto>> GetFriendPosts(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostDto> GetPost(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<PostDto>> GetUserPosts(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> LikePost(PostDto entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
