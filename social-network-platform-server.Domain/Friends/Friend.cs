4ab661fbad6359fbeeb52799a7d273d73d1d189c
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-04 11:51:23 +0600

    friend entity added

---
+++ social-network-platform-server.Domain/Friends/Friend.cs
@@ -0,0 +1,27 @@
+﻿using social_network_platform_server.Domain.AuditEntitys;
+using social_network_platform_server.Domain.Friends;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace social_network_platform_server.Domain.Friends
+{
+    public class Friend : AuditEntity
+    {
+        public int MyProperty { get; set; }
+        public Guid UserIdOne { get; set; }
+        public Guid UserIdTwo { get; set; }
+        public Status Status { get; set; }
+    }
+
+    public enum Status
+    {
+        pending = 1,
+        Declined = 2,
+        Accepted = 3
+    }
+
+}
