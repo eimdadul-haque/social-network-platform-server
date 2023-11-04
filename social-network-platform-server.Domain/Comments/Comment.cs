88ae80753a42c27373974c38fb03a2ac9c06ea09
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-04 11:50:07 +0600

    post entity added

---
+++ social-network-platform-server.Domain/Comments/Comment.cs
@@ -0,0 +1,22 @@
+﻿using social_network_platform_server.Domain.Accounts;
+using social_network_platform_server.Domain.AuditEntitys;
+using social_network_platform_server.Domain.Posts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Domain.Comments
+{
+    public class Comment : AuditEntity
+    {
+        public Guid? ParentCommentId { get; set; }
+        public Guid AuthorID { get; set; }
+        public string Content { get; set; }
+        public DateTime CommentDate { get; set; }
+        public Guid PostID { get; set; }
+        [ForeignKey("PostID")] public Post Post { get; set; }
+    }
+}
---
+++ social-network-platform-server.Domain/Posts/Post.cs
@@ -0,0 +1,21 @@
+﻿using social_network_platform_server.Domain.AuditEntitys;
+using social_network_platform_server.Domain.Comments;
+using social_network_platform_server.Domain.Likes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Domain.Posts
+{
+    public class Post : AuditEntity
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Guid AuthorID { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<Like> Likes { get; set; }
+    }
+}
