690678f0d8b5b33d09421834b4308177d958f769
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-04 11:52:45 +0600

    audit entity dto added

---
+++ social-network-platform-server.Application.Contracts/AuditEntitys/AuditEntityDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Application.Contracts
+{
+    public class AuditEntityDto<Tkey>
+    {
+        public Tkey Id { get; set; }
+        public DateTime Created { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime Modified { get; set; }
+        public string ModifiedBy { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
