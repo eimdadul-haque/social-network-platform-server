234d4aa3b0c4863a4ee3a2cfdfef37ce1bc5626d
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-04 11:51:04 +0600

    like entity added

---
+++ social-network-platform-server.Domain/Likes/Like.cs
@@ -0,0 +1,21 @@
+﻿using social_network_platform_server.Domain.AuditEntitys;
+using social_network_platform_server.Domain.Comments;
+using social_network_platform_server.Domain.Posts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Domain.Likes
+{
+    public class Like : AuditEntity
+    {
+        public Guid PostID { get; set; }
+        [ForeignKey("PostID")] public Post Post { get; set; }
+        public Guid CommentID { get; set; }
+        [ForeignKey("CommentID")] public Comment Comment { get; set; }
+        public Guid UserID { get; set; }
+    }
+}
