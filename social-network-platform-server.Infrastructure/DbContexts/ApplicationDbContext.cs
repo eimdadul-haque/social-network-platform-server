d13729e2a213d1224808f26406b4b9730d00177f
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-04 19:45:20 +0600

    DbContext added

---
+++ social-network-platform-server.Infrastructure/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using social_network_platform_server.Domain.Posts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Infrastructure.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) {}
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
