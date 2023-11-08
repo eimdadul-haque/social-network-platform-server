321931553bfc639e905a40283d321759cea7fb57
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-09 00:26:32 +0600

    auto mapper profile added

---
+++ social-network-platform-server.Application/AutoMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using social_network_platform_server.Application.Contracts.Posts.Dtos;
+using social_network_platform_server.Domain.Posts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Application
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap <Post, PostDto>().ReverseMap();
+        }
+    }
+}
