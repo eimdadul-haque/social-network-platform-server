e2bb717d225a0bc53fa752e41639cf466bd2357a
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-09 00:26:54 +0600

    post controller added

---
+++ social-network-platform-server.Api/Controllers/Commands/PostController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using social_network_platform_server.Application.Contracts.Posts.Dtos;
+using social_network_platform_server.Application.Contracts.Posts.Interfaces;
+
+namespace social_network_platform_server.Api.Controllers.Commands
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostService _postService;
+        public PostController(
+            IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpPost("CreatePost")]
+        public async Task<IActionResult> CreatePost(PostDto input)
+        {
+            var a = await _postService.CreatePost(input);
+            return Ok();
+        }
+    }
+}
