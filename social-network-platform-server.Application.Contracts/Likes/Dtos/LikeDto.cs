e503d5cb357ce4b009d0261ecb90aee2d58825f6
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-14 00:40:14 +0600

    like dto added

---
+++ social-network-platform-server.Application.Contracts/Likes/Dtos/LikeDto.cs
@@ -0,0 +1,18 @@
+﻿using social_network_platform_server.Application.Contracts.Comments.Dtos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Application.Contracts.Likes.Dtos
+{
+    public class LikeDto : AuditEntityDto<Guid>
+    {
+        public Guid PostID { get; set; }
+        public Guid CommentID { get; set; }
+        public CommentDto Comment { get; set; }
+        public Guid UserID { get; set; }
+    }
+}
