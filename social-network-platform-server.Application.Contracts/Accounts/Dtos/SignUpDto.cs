b5bdd6f9356dc49fef77e1534744bfd6e254eb0d
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-17 19:45:17 +0600

    SignUpDto added

---
+++ social-network-platform-server.Application.Contracts/Accounts/Dtos/SignUpDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_network_platform_server.Application.Contracts.Accounts.Dtos
+{
+    public class SignUpDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Usename { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
