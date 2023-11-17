f9d64b8ff403abae3391650f48c0c27208766afd
Eimdadul Haque <eimdadulhaque@gmail.com>
2023-11-17 19:24:13 +0600

    added identity framwork core

--- social-network-platform-server.Api/Program.cs
+++ social-network-platform-server.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using social_network_platform_server.Api.middlewares;
@@ -5,10 +7,11 @@ using social_network_platform_server.Application.Contracts.Posts.Interfaces;
 using social_network_platform_server.Application.Contracts.Repository.Interfaces;
 using social_network_platform_server.Application.Posts;
 using social_network_platform_server.Application.Repositorys;
+using social_network_platform_server.Domain.Accounts;
 using social_network_platform_server.Infrastructure.DbContexts;
 
+// Add services to the container.
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -20,19 +23,24 @@ builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddTransient<IPostService, PostService>();
 builder.Services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
 
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+});
 
-// Add services to the container.
-
-builder.Services.AddControllers();
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.File("log.txt")
     .CreateLogger();
 
-var app = builder.Build();
-
 // Configure the HTTP request pipeline.
+var app = builder.Build();
 
 if (app is not null)
 {
@@ -41,11 +49,8 @@ if (app is not null)
 }
 
 app.UseMiddleware<GlobalErrorHandler>();
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
